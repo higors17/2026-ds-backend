d5ce1778374b8a91fd3c00ab688d93b66841547b
Higor <hsilvapereira1@gmail.com>
2026-04-27 22:09:09 -0300

    aula nova
    atualizações da api

--- src/server.js
+++ src/server.js
@@ -6,7 +6,23 @@ function callbackDaRaiz( request, response ) {
    response.send("Vai Corinthians!!")
 }
 
+function callbackListaUsuarios(request, response) {
+    const listaUsuarios = [
+        {
+        id: 1,
+        name: "João"
+    }
+
+    ] 
+
+
+    response.json (listaUsuarios)
+
+}
+
 app.get("/", callbackDaRaiz)
+
+app.get("/users", callbackListaUsuarios)
 
 app.listen(3000, () => {
     console.log("API rodando em http://localhost:3000")
